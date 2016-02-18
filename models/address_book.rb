e81d3273109c3d716aa653bbf49c450d7b65b2fb
cpatel47 <chirag47@gmail.com>
2016-02-18 10:43:44 +0000

    checkpoint 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,11 +1,12 @@
  require_relative 'entry'
- 
+  require "csv"
+
  class AddressBook
   attr_reader :entries
   
   def initialize
      @entries = []
-   end
+  end
    
    def add_entry(name, phone_number, email)
        
@@ -14,7 +15,7 @@
          
           if name < entry.name
          break
-       end
+          end
        index += 1
      end
      
@@ -30,7 +31,16 @@
           end
        end
        @entries.delete(delete_entry)
+   end
+  
+  def import_from_csv(file_name)
+   csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+ # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
   end
-
    
  end
